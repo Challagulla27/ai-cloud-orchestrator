3497f13531b4419d4c223127a9832de4687998ef
vchallagulla <vchallagulla@iee.fraunhofer.de>
2026-04-10 17:11:06 +0200

    made some changes

--- server/tasks.py
+++ server/tasks.py
@@ -2,13 +2,20 @@ def easy_task(state):
     cpu = state["cpu_usage"]
     if 40 <= cpu <= 70:
         return 1
+    elif 30 <= cpu <= 80:
+        return 0.5    # Close but not perfect
     return 0
 
 
 def medium_task(state):
-    if state["cpu_usage"] < 80 and state["servers"] <= 3:
-        return 1
-    return 0
+    cpu = state["cpu_usage"]
+    servers = state["servers"]
+    score = 0
+    if cpu < 80:
+        score += 0.5
+    if servers <= 3:
+        score += 0.5
+    return score
 
 
 def hard_task(state):
@@ -17,4 +24,8 @@ def hard_task(state):
 
     if 40 <= cpu <= 70 and servers <= 3:
         return 1
+    elif 40 <= cpu <= 70 and servers > 3:
+        return 0.5
+    elif cpu < 80 and servers <= 3:
+        return 0.3
     return 0
